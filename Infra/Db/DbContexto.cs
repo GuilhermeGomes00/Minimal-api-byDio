4ec1ad283649e0402cfa009d80dc55b73d001ac7
Guilherme Gomes <guigomes0307@gmail.com>
2025-08-28 20:12:38 -0300

    Criação do banco de dados, criação do perfil ADM.

--- Infra/Db/DbContexto.cs
+++ Infra/Db/DbContexto.cs
@@ -1,6 +1,28 @@
-﻿namespace minimal_api_DIO.Infra.Db;
+﻿using Microsoft.EntityFrameworkCore;
+using minimal_api_DIO.Dominio.Entidades;
 
-public class DbContexto
+namespace minimal_api_DIO.Infra.Db;
+
+public class DbContexto : DbContext
 {
+    public DbSet<Administrador> Administradores { get; set; } =  default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {  
+                Id = 1,
+                Email = "Adm@teste.com",
+                Senha = "123456",
+                Perfil = "Adm"
+            });
+    }
     
-}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Administrador.Sqlite");
+        base.OnConfiguring(optionsBuilder);
+    }
+    
+} 
--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using minimal_api_DIO.Dominio.DTOs;
+using minimal_api_DIO.Infra.Db;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseSqlite(
+        "Data Source=Administrador.Sqlite");
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.MapGet("/teste", () => "Hello World!");
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "Adm@teste.com" && loginDTO.Senha == "123456") 
+        return Results.Ok("Login feito com sucesso!");
+    else 
+        return Results.Unauthorized();
+});
 
 app.Run();
