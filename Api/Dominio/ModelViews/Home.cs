e1a426420c4c213c66ab6dcc10d2e67966b882df
Guilherme Gomes <guigomes0307@gmail.com>
2025-09-25 19:07:45 -0300

    Home att.

--- Api/Dominio/ModelViews/Home.cs
+++ Api/Dominio/ModelViews/Home.cs
@@ -3,7 +3,16 @@
 public struct Home
 {
     // Apenas para verificar se funcionou!
-    public string? FOI { get => "FOI?"; }
     public string Mensagem { get => "Bem-Vindo a API de veículos - Minimal API"; }
-    public string Doc {get => "/swagger";}
+    public String Instruções
+    {
+        get => "Para ter acesso total, use o Email: 'Adm@teste.com' com a senha '123456', use o Token para não ter conflito" +
+               " de autorização.";
+    }
+    
+    public string Doc {get => "Acesse o Swagger aqui:";}
+    public string API
+    {
+        get => "/swagger";
+    }
 }
